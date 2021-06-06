9b1849fba66b2a04375190cde3c5c7810883e0e5
Cedar Bishop <>
2021-06-06 14:52:34 +0000

    Added commands to be able to add and delete encouraging message to encouragement list

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import os
 import discord
 from commands import *
 import random
+from replit import db
 
 
 client = discord.Client()
@@ -15,6 +16,20 @@ starter_encouragements = [
   "You are a great person!"
 ]
 
+def update_encouragements(encouraging_message):
+  if "encouragements" in db.keys():
+    encouragements = db["encouragements"]
+    encouragements.append(encouraging_message)
+    db["encouragements"] = encouragements
+  else:
+    db["encouragements"] = [encouraging_message]
+
+def delete_encouragement(index):
+  encouragements = db["encouragements"]
+  if len (encouragements) > index:
+    del encouragements[index]
+  db["encouragements"] = encouragements
+
 @client.event
 async def on_ready():
   print("We have logged in as {0.user}".format(client))
@@ -23,12 +38,39 @@ async def on_ready():
 async def on_message(message):
   if message.author == client.user:
     return
-  
-  if message.content.startswith('!greet'):
+
+  msg = message.content
+
+  options = starter_encouragements
+  if "encouragements" in db.keys():
+    for enc in db["encouragements"]:
+      options.append(enc)
+
+  if msg.startswith('!greet'):
     await greet(message, client)
 
-  if message.content.startswith('!quote'):
+  if msg.startswith('!quote'):
     await quote(message, client)
+
+  if msg.startswith('!new'):
+    encouraging_message = msg.split("!new ",1)[1]
+    update_encouragements(encouraging_message)
+    await message.channel.send("New encouraging message added")
+
+  if msg.startswith('!delete'):
+    encouragements = []
+    if "encouragements" in db.keys():
+      index = int(msg.split("!delete ",1)[1])
+      delete_encouragement(index)
+      encouragements = db["encouragements"]
+    await message.channel.send(encouragements)
+
+  if msg.startswith('!all'):
+    encouragements = db["encouragements"]
+    await message.channel.send(encouragements)
+
+  if any(word in msg for word in sad_words):
+    await message.channel.send(random.choice(options))
 
 my_secret = os.environ['TOKEN']
 client.run(my_secret)
